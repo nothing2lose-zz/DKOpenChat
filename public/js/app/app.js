8d717ba09fa554b0b787ab9c14cc330bc1c52eef
nothing <nothing.sungpil@gmail.com>
2015-09-01 22:55:40 +0900

    prepare pagination

--- public/js/app/app.js
+++ public/js/app/app.js
@@ -3,7 +3,7 @@
 
 // Declare app level module which depends on filters, and services
 //angular.module('DKOpenChat', ['DKOpenChat.filters', 'DKOpenChat.services', 'DKOpenChat.directives','ngRoute']).
-angular.module('DKOpenChat', ['ngRoute']).
+angular.module('DKOpenChat', ['ui.bootstrap', 'ngRoute', 'angularUtils.directives.dirPagination']).
   config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider.when('/', { controller: AppCtrl });
     //$routeProvider.when('/view2', {templateUrl: 'partial/2', controller: RoomCtrl});
--- public/js/app/controllers.js
+++ public/js/app/controllers.js
@@ -51,4 +51,17 @@ app.controller("AppCtrl", function($scope, $http, apiService) {
   });
 
 
+    // pagination control
+    $scope.totalItems = 64;
+    $scope.currentPage = 4;
+    $scope.maxSize = 5;
+
+    $scope.setPage = function (pageNo) {
+        $scope.currentPage = pageNo;
+    };
+
+    $scope.bigTotalItems = 175;
+    $scope.bigCurrentPage = 1;
+
+
 });
