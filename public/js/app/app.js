4480cf19f9ce806f881753a16feaa075e4681379
nothing <nothing.sungpil@gmail.com>
2015-09-05 04:31:01 +0900

    fix router

--- public/js/app/app.js
+++ public/js/app/app.js
@@ -5,7 +5,7 @@
 //angular.module('DKOpenChat', ['DKOpenChat.filters', 'DKOpenChat.services', 'DKOpenChat.directives','ngRoute']).
 angular.module('DKOpenChat', ['ui.bootstrap', 'ngRoute', 'angularUtils.directives.dirPagination']).
   config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-    $routeProvider.when('/', { controller: AppCtrl });
+    //$routeProvider.when('/', { controller: AppCtrl });
     //$routeProvider.when('/view2', {templateUrl: 'partial/2', controller: RoomCtrl});
     $routeProvider.otherwise({redirectTo: '/'});
     $locationProvider.html5Mode(true);
